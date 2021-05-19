ac6a9ca02b9cd7acfd21c1eb1e61092a21fe1efe
Lonnie Mitchell <lonniejmitchell@gmail.com>
2021-05-19 14:05:06 -0500

    still playing and learning

---
+++ style/imgslider.js
@@ -0,0 +1,24 @@
+
+// variables*/
+
+var carName= "ford truck";
+
+// objects */
+//properties*/
+var truck={
+id: 01,
+name: "Ranger",
+doors: 4,
+color: "red",
+//method*/
+
+carMove: function(){
+    //move*/
+}
+}
+
+var btn = document.getElementById("demo");
+btn.removeEventListener("click", function () {
+    console.log("button click");
+})
+
